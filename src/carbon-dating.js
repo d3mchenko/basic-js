9e67c53c84648c07dc2ee91b80d4522bc6e6f4af
d3mchenko <maksim-ignatyuk@mail.ru>
2021-03-29 18:18:28 +0300

    fix: update carbot-dating.js

--- src/carbon-dating.js
+++ src/carbon-dating.js
@@ -3,7 +3,10 @@ const CustomError = require("../extensions/custom-error");
 const MODERN_ACTIVITY= 15; 
 const HALF_LIFE_PERIOD= 5730;
 
-module.exports = function dateSample(/* sampleActivity */) {
-  throw new CustomError('Not implemented');
-  // remove line with error and write your code here
+module.exports = function dateSample(sampleActivity) {
+  let number = +sampleActivity;
+  if(typeof sampleActivity!=='string' || number == '' || number == ' ' || isNaN(number) || number <=0 || number >=15) return false;
+  let res = (Math.log(MODERN_ACTIVITY)-Math.log(number)) * (HALF_LIFE_PERIOD/0.693);
+  res = Math.ceil(res);
+  return res;
 };
